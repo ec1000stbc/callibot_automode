0bb4f73267b06335e7f31ca28dd31cc2d4f973f1
ec1000stbc <ch.raba@gmx.de>
2026-02-13 17:30:23 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,9 +3,14 @@ basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         start = 1
     }
+    if (input.buttonIsPressed(Button.B)) {
+        start = 0
+    }
     if (start == 1) {
         if (maqueen.ultrasonic(maqueen.DistanceUnit.Centimeters) < 15) {
             maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 100)
+            basic.pause(500)
+            maqueen.motorStop(maqueen.Motors.All)
             basic.pause(500)
             maqueen.motorStop(maqueen.Motors.M2)
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
